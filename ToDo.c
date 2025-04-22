45ebca864bd20e90c6e4e772a8a6f0f5b1bddbf2
AgustinJNadal <nadaljorge32@gmail.com>
2025-04-21 23:47:36 -0300

    creacion de la funcion crearNodo

--- ToDo.c
+++ ToDo.c
@@ -14,14 +14,44 @@ struct {
     Nodo *Siguiente;
 }typedef Nodo;
 
-Nodo * CrearListaVacia(); 
+Nodo * CrearListaVacia();
+Nodo * CrearNodo(int *id);
+
 
 int main() {
+    int id = 1000;
     Nodo * TareasPendientes = CrearListaVacia();
     Nodo * TareasRealizadas = CrearListaVacia();
+
+    Nodo * NuevaTarea = CrearNodo(&id);
+
     return 0;
 }
 
 Nodo * CrearListaVacia(){
     return NULL;
+}
+
+Nodo * CrearNodo(int *id){
+    Nodo * NuevaTarea = (Nodo *) malloc(sizeof(Nodo));
+    
+    NuevaTarea->Tarea.TareaID = (*id)++;
+
+    char desc[100];
+    printf("Ingrese descripcion: ");
+    fflush(stdin);
+    fgets(desc, 100, stdin);
+    desc[strcspn(desc, "\n")] = 0;
+    NuevaTarea->Tarea.Descripcion = (char *) malloc(strlen(desc) + 1);
+    strcpy(NuevaTarea->Tarea.Descripcion, desc);
+
+    do {
+        printf("Ingrese la duracion (entre 10 y 100): ");
+        scanf("%d", &NuevaTarea->Tarea.Duracion);
+    } while (NuevaTarea->Tarea.Duracion < 10 || NuevaTarea->Tarea.Duracion > 100);
+
+    
+    NuevaTarea->Siguiente = NULL;
+    
+    return NuevaTarea;
 }
